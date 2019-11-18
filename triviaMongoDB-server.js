87d555fef9a3f3f5f901872c31b150df2fced3c2
Way <wei.xw.xu@gmail.com>
2019-11-18 13:05:19 -0500

    updated /savequiz router

--- triviaMongoDB-server.js
+++ triviaMongoDB-server.js
@@ -21,6 +21,7 @@ let catArr = [];  //category array
 let difArr = [];  //difficulty array
 let queArr = [];  //question array
 let quizArr = []; //quiz array
+let quizFromDB = []; //quizzes from databse
 
 app.get('/', function(req, res){
   res.render('pages/index');
@@ -141,8 +142,8 @@ app.get('/questions', function(req, res){
       // client.close();
     }
   });
-  console.log('submitted1');
-  console.log(req.body);
+  // console.log('submitted1');
+  // console.log(req.body);
 
 });
 
@@ -203,8 +204,8 @@ app.get('/loadQs', function(req, res){
 // router for Add to Quiz in the createquiz page
 app.get('/addToQuiz', function(req, res){
   //testing
-  console.log("quizArr at the beginning of addToQuiz")
-  console.log(quizArr);
+  // console.log("quizArr at the beginning of addToQuiz")
+  // console.log(quizArr);
 
   loadDropDown();
   loadDatabaseQs(req, res);
@@ -224,13 +225,13 @@ app.get('/addToQuiz', function(req, res){
   if(!Array.isArray(toBeAdded)){
     toBeAdded = [toBeAdded];
   }
-  console.log("toBeAdded is:")
-  console.log(toBeAdded);
+  // console.log("toBeAdded is:")
+  // console.log(toBeAdded);
 
   // TODO: add to quizArr
   for(i in toBeAdded){
     console.log("looping through toBeAdded");
-    if(!quizArr.includes(toBeAdded[i])){
+    if(!quizArr.includes(JSON.parse(toBeAdded[i]))){
       quizArr.push(JSON.parse(toBeAdded[i]));
     }
     // mc.connect("mongodb://localhost:27017/", function(err, client) {
@@ -249,10 +250,105 @@ app.get('/addToQuiz', function(req, res){
   }
 
   //testing
-  console.log("quizArr at the end of loadQs")
-  console.log(quizArr);
+  // console.log("quizArr at the end of loadQs")
+  // console.log(quizArr);
   res.status(200).render('pages/createquiz', {category:catArr, difficulty:difArr, quizArray:quizArr, queArray:queArr});
 })
+
+app.get('/remFromQuiz', function(req, res){
+  loadDropDown();
+  loadDatabaseQs(req, res);
+
+  let toBeRemoved = req.query.toBeRemoved;
+  if(!Array.isArray(toBeRemoved)){
+    toBeRemoved = [toBeRemoved];
+  }
+  // console.log("toBeRemoved is:")
+  // console.log(toBeRemoved);
+
+  // TODO: add to quizArr
+  for(i in toBeRemoved){
+    for(j in quizArr){
+      if(JSON.stringify(quizArr[j]) == toBeRemoved[i]){
+        delete quizArr[j];
+        break;  // jumps to the next question
+      }
+    }
+    // //console.log("looping through toBeRemoved");
+    // if(!queArr.includes(toBeRemoved[i])){
+    //   queArr.push(JSON.parse(toBeRemoved[i]));
+    // }
+  }
+  // console.log("quizArr at the end of toBeRemoved is: ");
+  // console.log(quizArr);
+  res.status(200).render('pages/createquiz', {category:catArr, difficulty:difArr, quizArray:quizArr, queArray:queArr});
+
+})
+
+app.get('/savequiz', function(req, res){
+  let creator = req.query.creator.toLowerCase();
+  // console.log(req.query.tags);
+  let tags = req.query.tags.split(" ");
+
+  for(i in tags){
+    tags[i] = tags[i].toLowerCase();
+  }
+
+  console.log(creator);
+  console.log(tags);
+
+  // if fields are not filled out
+  if(typeof creator == "undefined" || typeof tags == "undefined"){
+    alert("Please fill in the creator and tags before saving");
+    return;
+  }
+
+  if(quizArr.length == 0){
+    alert("Please select some questions to add");
+    return;
+  }
+
+  //create a database collection for quizes
+  let quiz = {
+    creator: creator,
+    tags: tags,
+    questions: quizArr
+  }
+
+  //add to quizzes db
+  mc.connect("mongodb://localhost:27017", function(err, client) {
+  	if (err) {
+  		console.log("Error in connecting to database");
+  		console.log(err);
+  		return;
+  	}
+    db = client.db('a4');
+    db.collection("quizzes").insertOne(quiz, function(err, result){
+      if(err) throw err;
+      console.log("Successfuly inserted quiz.")
+    });
+
+    // load the quizzes from the databse and render it to page
+    db.collection("quizzes").find({}).toArray(function(err, docs){
+      if (err){
+        throw err;
+      }
+      //is this necessary?
+      quizFromDB = docs.slice();
+      // if(quizzes.length > 25){
+      //   queArr = queArr.slice(0, 25);
+      // }
+      console.log("quizFromDB is: ");
+      console.log(quizFromDB);
+      res.status(200).render('pages/quizzes', {quizDB:quizFromDB});
+    });
+  });
+});
+
+app.get('/quizzes', function(req, res){
+  
+})
+
 
 function loadDropDown(){
   // getDropDownArr function
@@ -359,8 +455,6 @@ function loadDatabaseQs(req, res){
     }
   });
 }
-
-
 
 
 // console.log(app.locals);
